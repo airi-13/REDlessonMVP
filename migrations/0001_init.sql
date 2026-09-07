b125eca6936a29a54ee1a2653fc63d1ba1aa7d4d
airi-13 <iri130530@gmail.com>
2026-09-07 22:37:21 +0900

    Fix D1 migration seed query

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -32,10 +32,14 @@ CREATE TABLE weekday_availability (
   PRIMARY KEY (weekday, period)
 );
 
-INSERT INTO weekday_availability(weekday,period,available)
-SELECT w.weekday, p.period, 1
-FROM (SELECT 0 AS weekday UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6) w
-CROSS JOIN period_slots p;
+INSERT INTO weekday_availability(weekday,period,available) VALUES
+(0,1,1),(0,2,1),(0,3,1),(0,4,1),(0,5,1),(0,6,1),(0,7,1),(0,8,1),
+(1,1,1),(1,2,1),(1,3,1),(1,4,1),(1,5,1),(1,6,1),(1,7,1),(1,8,1),
+(2,1,1),(2,2,1),(2,3,1),(2,4,1),(2,5,1),(2,6,1),(2,7,1),(2,8,1),
+(3,1,1),(3,2,1),(3,3,1),(3,4,1),(3,5,1),(3,6,1),(3,7,1),(3,8,1),
+(4,1,1),(4,2,1),(4,3,1),(4,4,1),(4,5,1),(4,6,1),(4,7,1),(4,8,1),
+(5,1,1),(5,2,1),(5,3,1),(5,4,1),(5,5,1),(5,6,1),(5,7,1),(5,8,1),
+(6,1,1),(6,2,1),(6,3,1),(6,4,1),(6,5,1),(6,6,1),(6,7,1),(6,8,1);
 
 CREATE TABLE weekly_lessons (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
